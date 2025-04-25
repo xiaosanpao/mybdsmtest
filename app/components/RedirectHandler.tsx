732ec6eb5d6af200fd0f803ec2c13d64823a4e28
xiaosanpao <xuelunzhichi@gmail.com>
2025-04-25 15:25:13 +0800

    Update RedirectHandler.tsx

--- app/components/RedirectHandler.tsx
+++ app/components/RedirectHandler.tsx
@@ -1,1 +1,13 @@
+'use client';
 
+import { useEffect } from 'react';
+
+export default function RedirectHandler() {
+  useEffect(() => {
+    if (typeof window !== 'undefined' && window.location.protocol === 'http:') {
+      window.location.href = window.location.href.replace('http:', 'https:');
+    }
+  }, []);
+
+  return null;
+}
